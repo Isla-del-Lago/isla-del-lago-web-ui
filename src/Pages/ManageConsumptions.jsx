9d5ed65850d7452fb72d5300ec9262d5c27be12b
Alexander Lansdorp <alexlansdorprs@gmail.com>
2022-06-17 15:30:23 -0500

    Loader bugs fixed, double search in manage consumptions page disabled

--- src/Pages/ManageConsumptions.jsx
+++ src/Pages/ManageConsumptions.jsx
@@ -20,7 +20,7 @@ let consumptions = [];
 let currentApartment = '';
 export default function ManageConsumptions() {
     const authCtx = useContext(AuthContext);
-    const [loaderVisibility, setLoaderVisibility] = useState('invisible');
+    const [loaderVisibility, setLoaderVisibility] = useState(false);
     const [searched, setSearched] = useState(false);
 
     const today = new Date();
@@ -95,147 +95,154 @@ export default function ManageConsumptions() {
         dates = [];
         consumptions = [];
         costs = [];
-        setLoaderVisibility('visible');
-        fetch(
-            `${process.env.REACT_APP_BILL_URL}/api/v1/bill/billDate?startDate=${enteredStartDate}&endDate=${enteredEndDate}`,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': utils.headers['Content-Type'],
-                    'X-Uuid': sessionStorage.getItem('X-uuid'),
-                    Authorization: sessionStorage.getItem('Token'),
-                },
-            }
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.billId) {
-                    setCurrentBill(data);
-                    currentApartment = enteredApartmentId;
-                    fetch(
-                        `${process.env.REACT_APP_BILL_URL}/api/v1/bill/consumption/consumptionDetail/apartmentId/${enteredApartmentId}/billId/${data.billId}`,
-                        {
-                            method: 'GET',
-                            headers: {
-                                'Content-Type': utils.headers['Content-Type'],
-                                'X-Uuid': sessionStorage.getItem('X-uuid'),
-                                Authorization: sessionStorage.getItem('Token'),
-                            },
-                        }
-                    )
-                        .then((response) => response.json())
-                        .then((data) => {
-                            setSearched(true);
-                            setCurrentConsumption(data);
-                            setLoaderVisibility('invisible');
-                            if (data.error) {
-                                setSearched(false);
-                                setLoaderVisibility('invisible');
-                                Swal.fire({
-                                    title: data.errorCode,
-                                    text: data.error,
-                                    icon: 'warning',
-                                    confirmButtonText: 'Continuar',
-                                });
-                            }
-                        })
-                        .catch((error) => {
-                            setSearched(false);
-                            setLoaderVisibility('invisible');
-                            Swal.fire({
-                                title: 'Error!' + error.State,
-                                text: error.error,
-                                icon: 'error',
-                                confirmButtonText: 'Continuar',
-                                allowEscapeKey: false,
-                                allowOutsideClick: false,
-                            });
-                        });
-                    fetch(
-                        `${process.env.REACT_APP_BILL_URL}/api/v1/bill/consumption/consumptionDetail/apartmentId/${enteredApartmentId}`,
-                        {
-                            method: 'GET',
-                            headers: {
-                                'Content-Type': utils.headers['Content-Type'],
-                                'X-Uuid': sessionStorage.getItem('X-uuid'),
-                                Authorization: sessionStorage.getItem('Token'),
-                            },
-                        }
-                    )
-                        .then((response) => response.json())
-                        .then((data) => {
-                            if (data) {
-                                Object.entries(data)
-                                    .sort()
-                                    .forEach((element) => {
-                                        dates.push(element[0]);
-                                        consumptions.push(
-                                            element[1]
-                                                .residentialBasicCubicMeters +
-                                                element[1]
-                                                    .residentialBasicSuperiorCubicMeters
-                                        );
-                                        costs.push(element[1].total);
-                                    });
-                                setBillDates(dates);
-                                setBillConsumptions(consumptions);
-                                setBillCosts(costs);
-                            }
-                            setLoaderVisibility('invisible');
-                            if (data.error) {
-                                setSearched(false);
-                                setLoaderVisibility('invisible');
-                                Swal.fire({
-                                    title: data.errorCode,
-                                    text: data.error,
-                                    icon: 'warning',
-                                    confirmButtonText: 'Continuar',
-                                });
-                            }
-                        })
-                        .catch((error) => {
-                            setSearched(false);
-                            setLoaderVisibility('invisible');
-                            Swal.fire({
-                                title: 'Error!' + error.State,
-                                text: error.error,
-                                icon: 'error',
-                                confirmButtonText: 'Continuar',
-                                allowEscapeKey: false,
-                                allowOutsideClick: false,
-                            });
-                        });
+        if (currentApartment !== enteredApartmentId) {
+            setSearched(false);
+            setLoaderVisibility(true);
+            fetch(
+                `${process.env.REACT_APP_BILL_URL}/api/v1/bill/billDate?startDate=${enteredStartDate}&endDate=${enteredEndDate}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': utils.headers['Content-Type'],
+                        'X-Uuid': sessionStorage.getItem('X-uuid'),
+                        Authorization: sessionStorage.getItem('Token'),
+                    },
                 }
-                if (data.error) {
+            )
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.billId) {
+                        setCurrentBill(data);
+                        currentApartment = enteredApartmentId;
+                        fetch(
+                            `${process.env.REACT_APP_BILL_URL}/api/v1/bill/consumption/consumptionDetail/apartmentId/${enteredApartmentId}/billId/${data.billId}`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    'Content-Type':
+                                        utils.headers['Content-Type'],
+                                    'X-Uuid': sessionStorage.getItem('X-uuid'),
+                                    Authorization:
+                                        sessionStorage.getItem('Token'),
+                                },
+                            }
+                        )
+                            .then((response) => response.json())
+                            .then((data) => {
+                                setSearched(true);
+                                setCurrentConsumption(data);
+                                setLoaderVisibility(false);
+                                if (data.error) {
+                                    setSearched(false);
+                                    setLoaderVisibility(false);
+                                    Swal.fire({
+                                        title: data.errorCode,
+                                        text: data.error,
+                                        icon: 'warning',
+                                        confirmButtonText: 'Continuar',
+                                    });
+                                }
+                            })
+                            .catch((error) => {
+                                setSearched(false);
+                                setLoaderVisibility(false);
+                                Swal.fire({
+                                    title: 'Error!' + error.State,
+                                    text: error.error,
+                                    icon: 'error',
+                                    confirmButtonText: 'Continuar',
+                                    allowEscapeKey: false,
+                                    allowOutsideClick: false,
+                                });
+                            });
+                        fetch(
+                            `${process.env.REACT_APP_BILL_URL}/api/v1/bill/consumption/consumptionDetail/apartmentId/${enteredApartmentId}`,
+                            {
+                                method: 'GET',
+                                headers: {
+                                    'Content-Type':
+                                        utils.headers['Content-Type'],
+                                    'X-Uuid': sessionStorage.getItem('X-uuid'),
+                                    Authorization:
+                                        sessionStorage.getItem('Token'),
+                                },
+                            }
+                        )
+                            .then((response) => response.json())
+                            .then((data) => {
+                                if (data) {
+                                    Object.entries(data)
+                                        .sort()
+                                        .forEach((element) => {
+                                            dates.push(element[0]);
+                                            consumptions.push(
+                                                element[1]
+                                                    .residentialBasicCubicMeters +
+                                                    element[1]
+                                                        .residentialBasicSuperiorCubicMeters
+                                            );
+                                            costs.push(element[1].total);
+                                        });
+                                    setBillDates(dates);
+                                    setBillConsumptions(consumptions);
+                                    setBillCosts(costs);
+                                }
+                                setLoaderVisibility(false);
+                                if (data.error) {
+                                    setSearched(false);
+                                    setLoaderVisibility(false);
+                                    Swal.fire({
+                                        title: data.errorCode,
+                                        text: data.error,
+                                        icon: 'warning',
+                                        confirmButtonText: 'Continuar',
+                                    });
+                                }
+                            })
+                            .catch((error) => {
+                                setSearched(false);
+                                setLoaderVisibility(false);
+                                Swal.fire({
+                                    title: 'Error!' + error.State,
+                                    text: error.error,
+                                    icon: 'error',
+                                    confirmButtonText: 'Continuar',
+                                    allowEscapeKey: false,
+                                    allowOutsideClick: false,
+                                });
+                            });
+                    }
+                    if (data.error) {
+                        setSearched(false);
+                        setLoaderVisibility(false);
+                        Swal.fire({
+                            title: data.errorCode,
+                            text: data.error,
+                            icon: 'warning',
+                            confirmButtonText: 'Continuar',
+                            allowEscapeKey: false,
+                            allowOutsideClick: false,
+                        });
+                    }
+                })
+                .catch((error) => {
                     setSearched(false);
-                    setLoaderVisibility('invisible');
+                    setLoaderVisibility(false);
                     Swal.fire({
-                        title: data.errorCode,
-                        text: data.error,
-                        icon: 'warning',
+                        title: 'Error!' + error.State,
+                        text: error.error,
+                        icon: 'error',
                         confirmButtonText: 'Continuar',
                         allowEscapeKey: false,
                         allowOutsideClick: false,
                     });
-                }
-            })
-            .catch((error) => {
-                setSearched(false);
-                setLoaderVisibility('invisible');
-                Swal.fire({
-                    title: 'Error!' + error.State,
-                    text: error.error,
-                    icon: 'error',
-                    confirmButtonText: 'Continuar',
-                    allowEscapeKey: false,
-                    allowOutsideClick: false,
                 });
-            });
+        }
     };
 
     return (
         <React.Fragment>
-            <Loader visible={loaderVisibility} />
+            {loaderVisibility && <Loader />}
             {authCtx.userLoginState && (
                 <Form className='customForm' onSubmit={submitHandler}>
                     <div className='customForm--title'>
@@ -261,7 +268,6 @@ export default function ManageConsumptions() {
                     />
                     <Input
                         onChange={setApartmentHandler}
-                        autoFocus={true}
                         type='text'
                         placeHolder='Apartamento'
                         id='apartmentId'
